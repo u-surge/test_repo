3fc2f39d5f5d7195e4901327bfb22537d3d4a26f
Jihyun-Kim <37255647+Jihyun-Jay-Kim@users.noreply.github.com>
2021-11-28 00:15:45 +0900

    kjh_kakao login_1
    kjh_kakao login_1

--- SampleApp/App.js
+++ SampleApp/App.js
@@ -38,69 +38,108 @@ import {
 } from 'react-native/Libraries/NewAppScreen';
 import { BOOLEAN_UNARY_OPERATORS } from '@babel/types';
 
-const signInWithKakao = async (): Promise<void> => {
-  const token: KakaoOAuthToken = await login();
-  setResult(JSON.stringify(token));
-};
+import { Modal, ModalContent, ModalPortal } from 'react-native-modals';
 
-const signOutWithKakao = async (): Promise<void> => {
-  const message = await logout();
+const LogInSection = ({ }): React.ReactElement => {
+  const [result, setResult] = useState<string>('');
+  
+  //State를 이용하여 Modal을 제어함
+  const [modalVisible, setModalVisible] = useState<boolean>(false);
+  //Output을 State로 받아서 화면에 표출하거나 정보 값으로 활용
+  const [modalOutput, setModalOutput] = useState<string>("Open Modal");
 
-  setResult(message);
-};
+  
+  const [user_id, setUserId] = useState();
+  const [nickName, setNickName] = useState();
+  
+  const signInWithKakao = async (): Promise<void> => {
+    const token: KakaoOAuthToken = await login();
 
-const getProfile = async (): Promise<void> => {
-  const profile: KakaoProfile = await getKakaoProfile();
+    setResult(JSON.stringify(token));
+  };
 
-  setResult(JSON.stringify(profile));
-};
+  const signOutWithKakao = async (): Promise<void> => {
+    const message = await logout();
+    setUserId("");
+    setNickName("");
 
-const unlinkKakao = async (): Promise<void> => {
-  const message = await unlink();
+    setResult(message);
+  };
 
-  setResult(message);
-};
+  const getProfile = async (): Promise<void> => {
+    const profile: KakaoProfile = await getKakaoProfile();
 
-const LogInSection = ({ }): Node => {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
+    setUserId(profile.id);
+    setNickName(profile.nickname);
+
+    setResult(JSON.stringify(profile));
+  };
+
+  const unlinkKakao = async (): Promise<void> => {
+    const message = await unlink();
+
+    setResult(message);
+  };
 
   return (
     <View style={styles.sectionContainer}>
+      <Modal
+        visible={modalVisible}
+        style={{alignItems: 'center'}}
+        onTouchOutside={() => {
+          setModalVisible(false);
+        }}
+      >
+        <ModalContent
+          style={{
+            flex: 1,
+            backgroundColor: 'fff',
+          }}
+        >
+          <Text>
+            {modalOutput}
+          </Text>
+        </ModalContent>
+      </Modal>
+
       <Text>
         LogIn 화면
       </Text>
       <Button
         onPress={() => {
-          setIsLoggedIn(true);
-          console.log("로그인 버튼 Clicked!");
-          signInWithKakao();
+        console.log("로그인 버튼 Clicked!");
+        signInWithKakao();
+        getProfile()
         }}
         title={"카카오로 로그인하기 버튼"}
       ></Button>
-            <Button
+      <Button
         onPress={() => {
-          setIsLoggedIn(true);
-          console.log("로그아웃 버튼 Clicked!");
-          signOutWithKakao();
+        console.log("로그아웃 버튼 Clicked!");
+        signOutWithKakao();
         }}
         title={"카카오로 로그아웃하기 버튼"}
       ></Button>
-            <Button
+      <Button
         onPress={() => {
-          setIsLoggedIn(true);
-          console.log("btn Clicked!");
-          unlinkKakao();
+        console.log("btn Clicked!");
+        unlinkKakao();
         }}
         title={"연결 끊기 버튼"}
       ></Button>
-            <Button
+      <Button
         onPress={() => {
-          setIsLoggedIn(true);
-          console.log("btn Clicked!");
-          console.log(getProfile());
+        console.log("btn Clicked!");
+        getProfile()
+        console.log(result);
+        //setModalOutput("카카오 프로필 조회 : id=" + user_id + ", nickname=" + nickName);
+        //setModalVisible(true);
+        Alert.alert("카카오 프로필 조회 : id=" + user_id + ", nickname=" + nickName)
         }}
         title={"카카오 프로필 받기 버튼"}
       ></Button>
+      
+
     </View>
   )
 }
@@ -124,7 +163,7 @@ const App: () => Node = () => {
             backgroundColor: isDarkMode ? Colors.black : Colors.white,
           }}>
             <LogInSection/>
-            
+            <ModalPortal />
           <Button
         //버튼의 타이틀을 정의하는 속성
         title="Press Me"
